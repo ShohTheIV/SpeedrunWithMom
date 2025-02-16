9e38bdae31591614fd027fcacc131c32bd4117e9
ShohTheIV <shoktheiv@gmail.com>
2025-02-16 19:23:52 +0500

    Initial commit

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform player;
+
+    void Update() {
+        transform.position = player.transform.position;
+    }
+}
